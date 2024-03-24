fd435268883e742a9dc6726acaea8629ac1b09db
BrunoG <bruno.guiomar33@gmail.com>
2024-03-24 22:11:32 +0000

    Slave Inference
    - Slave cant run inference

--- src/main/java/com/slave/application/Slave.java
+++ src/main/java/com/slave/application/Slave.java
@@ -4,6 +4,7 @@ import com.shared.SlaveInfo;
 import com.shared.SlaveService;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.net.*;
--- src/main/java/com/slave/application/SlaveServiceImpl.java
+++ src/main/java/com/slave/application/SlaveServiceImpl.java
@@ -3,8 +3,11 @@ package com.slave.application;
 import com.shared.JobFile;
 import com.shared.SlaveService;
 
+import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
@@ -35,26 +38,53 @@ public class SlaveServiceImpl extends UnicastRemoteObject implements SlaveServic
     }
 
     @Override
-    public boolean setupProblemEnvironment(ArrayList<JobFile> jobFiles) {
-        for (JobFile f : jobFiles) {
-            System.out.println(f.getName());
+    public boolean setupProblemEnvironment(ArrayList<JobFile> jobFiles, String problemCode) {
+        // Create Problem Folder
+        String problemDir = FilePathConstants.PROBLEM_PARAMS_FOLDER + problemCode;
+        File problemFolder = new File(problemDir);
+        problemFolder.mkdir();
+
+        // Create Problem Files
+        for (JobFile jobFile : jobFiles) {
+            try {
+                FileOutputStream fileOut = new FileOutputStream(problemDir + "/" + jobFile.getName());
+                fileOut.write(jobFile.getFileBytes());
+                fileOut.close();
+                System.out.println(jobFile.getName() + " file successfully created");
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("IO Exception while writing " + jobFile.getName() + " file");
+                return false;
+            }
         }
 
-        // Create .conf file
-        //try {
-        //    FileOutputStream fileOut = new FileOutputStream(jobFiles.get("CONF").getName());
-        //    fileOut.write(jobFiles.get("CONF").getFileBytes());
-        //    fileOut.close();
-        //    System.out.println(".conf file successfully written");
-        //} catch (IOException e) {
-        //    e.printStackTrace();
-        //    System.err.println("IO Exception while writing .conf file");
-        //    return false;
-        //}
+        // Create Slave params Files
+        String slaveParamsText = "parent.0 = slave.params" + System.lineSeparator() +
+                                 "parent.1 = " + problemCode + ".params";
+        File slaveParamsFile = new File(problemDir + "/" + problemCode + "Slave.params");
+        FileWriter fWriter;
+        try {
+            fWriter = new FileWriter(slaveParamsFile);
+            fWriter.write(slaveParamsText);
+            fWriter.close();
+            System.out.println("Successfully wrote the slave file");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("IO Exception");
+        }
 
-        // Create .params files
+        return true;
+    }
 
-
+    @Override
+    public boolean startInference(String problemCode) {
+        File paramsFile = new File(FilePathConstants.PROBLEM_PARAMS_FOLDER + problemCode + "/" + problemCode + "Slave.params");
+        System.out.println(paramsFile);
+        try {
+            ec.eval.Slave.main(new String[]{"-file", paramsFile.getCanonicalPath()});
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return true;
     }
 
